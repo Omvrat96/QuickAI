afc72b865e895b8e3d9209a6335a50f2be1775ce
Omvrat <omvrat969663@gmail.com>
2025-11-01 01:47:58 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
     <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
       
       <div className='text-center  mb-6'>
-                      <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create Amazing content <br /><span className='text-primary'>with AI</span></h1>
-                      <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'>Transform content creation ways by using AI to crete beautiful and enhaced content with AI images</p>
+                      <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create Amazing content <br /><span with className='text-primary'>with AI tools</span></h1>
+                      <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'>Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your workflow.</p>
                   </div>
                   <div className='flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
                       <button 
